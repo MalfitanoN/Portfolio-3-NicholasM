86e0f502c3570ceefa30f7f0aa5a978236a2bc09
Nicholas Malfitano <nicholas1malfitano@gmail.com>
2015-01-08 15:01:12 -0800

    yea

--- public_html/Script.js
+++ public_html/Script.js
@@ -1,4 +1,7 @@
 $("document").ready(function(){
     $(".Main").css("background-color", "blue");
     
+    $("div:nth-child(1)").css("border-style", "solid");
+    
+    $("p:gt(1)").css("border-style", "solid")
 });
